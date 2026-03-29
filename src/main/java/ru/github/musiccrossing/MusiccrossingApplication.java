444ffb90bd5dafc068e9e978bde5a05fb783be33
flowihs <zmulenko500@gmail.com>
2026-03-29 14:58:54 +0300

    init project

---
+++ src/main/java/ru/github/musiccrossing/MusiccrossingApplication.java
@@ -0,0 +1,11 @@
+package ru.github.musiccrossing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusiccrossingApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MusiccrossingApplication.class, args);
+    }
+}
